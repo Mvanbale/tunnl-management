5682e7066a6bc09ac80b947a4fdf660706f0f633
Michael van Bale <mvanbale@gmail.com>
2016-04-19 15:44:18 +0200

    Added viewing to react front end

--- src/js/statecomponents/stream-viewer-component.js
+++ src/js/statecomponents/stream-viewer-component.js
@@ -8,38 +8,99 @@ const streamRepository = require('../repositories/stream-repository');
 
 export default class StreamComponent extends React.Component {
 
-  constructor (props) {
+  constructor(props) {
     super(props);
+    this.assetURL;
+    this.mimeCodec;
+    this.counter = 0;
+    this.sourceBuffer;
+    this.mediaSource;
+    this.assetURL = `http://localhost:3000/${this.props.params.name}/getsegment/`;
+    this.mimeCodec = 'video/webm;codecs=vp8';
   }
 
-  componentDidMount () {
-    // this.streamRepository = streamRepository;
-    // this.streamRepository.getStreams()
-    //   .then((response) => {
-    //     console.log(response);
-    //     this.setState({
-    //       streams: response
-    //     });
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
-  }
-
-
-    render () {
-      //validate the present state here
-      if (true) {
-        return (
-        <div>
-        TODO: Insert the video player! <br></br>
-Should display the stream of {this.props.params.name}
-
-
-        </div>
-        );
+  componentDidMount() {
+    this.mediaSource = new window.MediaSource;
+    video.src = URL.createObjectURL(this.mediaSource);
+    setTimeout(function () {
+      if ('MediaSource' in window && window.MediaSource.isTypeSupported(this.mimeCodec)) {
+        this.mediaSource.addEventListener('sourceopen', this.sourceOpen(this));
       } else {
-        return (<div>Een ogenblik geduld...</div>);
+        console.error('Unsupported MIME type or codec: ', this.mimeCodec);
       }
+    }.bind(this), 800);
+
+
+  }
+
+
+  render() {
+    //validate the present state here
+    if (true) {
+      return (
+        <div>
+          <div class="camera">
+            <video id="video" autoPlay>Video stream not available.</video>
+          </div>
+          <canvas id="canvas">
+          </canvas>
+          <div class="output">
+
+          </div>
+        </div>
+      );
+    } else {
+      return (<div>Een ogenblik geduld...</div>);
     }
+  }
+
+
+  //meuk
+
+
+  playBlob() {
+    console.log('Playblob');
+  }
+
+  sourceOpen(self) {
+    if (!self.SourceBuffer) {
+      self.sourceBuffer = self.mediaSource.addSourceBuffer(self.mimeCodec);
+      self.sourceBuffer.addEventListener('updateend', function (_) {
+        self.mediaSource.endOfStream();
+        document.getElementById("video").play();
+        self.fetchAB(self.assetURL, self.resultCallBack.bind(self));
+      });
+
+      self.sourceBuffer.addEventListener('error', function (_) {
+        console.log(_);
+      })
+
+      self.fetchAB(self.assetURL, self.resultCallBack.bind(self));
+    }
+
+
+  };
+
+  resultCallBack(buf) {
+
+    this.sourceBuffer.appendBuffer(buf);
+  }
+
+  fetchAB(url, cb) {
+    var xhr = new XMLHttpRequest;
+    xhr.open('get', url + this.counter);
+    this.counter++;
+    xhr.responseType = 'arraybuffer';
+    xhr.onload = function () {
+      if (xhr.status == 200)
+        cb(xhr.response);
+    };
+    xhr.send();
+  };
+
+
+  playVideo(self) {
+    self.playBlob();
+  };
+
 }
